2078eff1c1fb412942d3e885cc3452e82a8213cf
d4kic <dakic.d.aleksandar@gmail.com>
2026-04-10 16:21:00 +0200

    ReqQ and cache dev

---
+++ zip-server/src/Cache/CacheItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zip_server.src.Cache
+{
+    internal class CacheItem
+    {
+        public byte[]? Data { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ zip-server/src/Cache/CacheManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zip_server.src.Cache
+{
+    internal class CacheManager
+    {
+    }
+}
